f9ebf7eed03bf1dbc43065a30a18f6716a2e7336
tyler <rowla070@morris.umn.edu>
2020-09-05 19:54:56 -0500

    "Add steps 3 & 4."

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -8,4 +8,10 @@ cd "$dir"|| exit
 #Failed login data was created in order to search for secure files
 #Which then outputs to failed_login_data.txt.
 cat var/log/*|awk
-awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n"  2. " groups[2] "\n" }' < failed_login_data.txt
+#awk 'match($0, /([a-zA-Z]+), ([a-zA-Z]+)/, groups) {print "1. " groups[1] "\n"  2. " groups[2] "\n" }' < failed_login_data.txt
+awk 'match($0, /([a-zA-Z]+):(Z+):(Z+) ([a-zA-Z]+) sshd\[(Z+)\]: Failed password for (invalid user )?([a-zA-Z._-]+) from ([Z.]+) port ([Z]+) ssh2/, groups)' <failed_login_data.txt
+
+#Removing seconds and minutes from the times
+sed ('s/seconds//' failed_login_data.sh, 's/minutes//' failed_login.sh)
+
+
