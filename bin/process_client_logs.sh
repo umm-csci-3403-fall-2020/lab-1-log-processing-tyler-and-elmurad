baf2902e66369815219472c971dd123ead69ef4f
tyler <rowla070@morris.umn.edu>
2020-09-11 13:15:03 -0500

    Complete process_client_logs

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 #takes a directory and sets it as dir variable
 dir=$1
@@ -15,7 +15,7 @@ cat var/log/* | awk -F "[, ]+" '/Failed password for/ && !/Failed password for i
 #cat var/log/*| awk -F "[, ]+" 'match(/Failed password for invalid user/ {print $1, $2, $3, $11, $13, /Failed password for/ && !/Failed password for invalid user/ {print $1, $2, $3, $9, $11})'
 
 #Removing seconds and minutes from the times
-sed -i .bak 's/:..//g' failed_login_data.txt
+sed -E -i=".bak" 's/:..//g' failed_login_data.txt
 
 #Return to main directory
 cd .. || exit
