24d8ae7b8f3c1f8172e33a973acd0fe7962ab73b
tyler <rowla070@morris.umn.edu>
2020-09-11 13:27:54 -0500

    Complete creat_hours_dist

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,27 +1,23 @@
 #!/usr/bin/env bash
 
-#Constant which initializes directory as a variable
-
-dir="$1"
-
 #Capturing current directory including failed login attempts using pwd
 
 main=$(pwd)
 
+#Redirects to another directory
 
+cd "$1"||exit
 
-cat ./*/failed_login_data.txt | awk '{print $3}' | sort | uniq -c | awk '{printf "data.addRow(["\x27$2\x27", "$1"]);"}' > hours_dist.html
+touch "hours_dist.txt"
+
+cat ./*/failed_login_data.txt | awk '{print $3}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' >> hours_dist.txt
 
 #Taking back to the top level search directory
 
 cd "$main" || exit
 
-cd html_components || exit
+#Placing wrap_contents files into username_dist
 
-#Placing wrap_content files into hours_dist
-../ wrap_contents.sh ../ "$1"/hours_dist.txt hours_dist hours_dist.html
+./bin/wrap_contents.sh "$1"/hours_dist.txt html_components/hours_dist "$1"/hours_dist.html
 
-mv hours_dist.html ./"$dir"
-
-#Remove directory
-rm "$dir"/hours_dist.txt
+rm "$1"/hours_dist.txt
